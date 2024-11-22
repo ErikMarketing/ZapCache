465ef90f19a062b8db66b68c910b17308fa82701
ErikMarketing <erik@aiwiz.uk>
2024-11-22 02:25:33 +0000

    Update zap-cache.php
    Minor version update - 1.2.1 for updated Hostinger/LiteSpeed host recognition logic.

--- zap-cache.php
+++ zap-cache.php
@@ -3,7 +3,7 @@
  * Plugin Name: Zap Cache
  * Plugin URI: https://github.com/ErikMarketing/zap-cache
  * Description: Lightning-fast cache cleaning with one click. Automatic detection for Hostinger, WP Engine, Kinsta, SiteGround, Cloudways, Nitropack and major caching plugins
- * Version: 1.2.0
+ * Version: 1.2.1
  * Author: ErikMarketing
  * Author URI: https://erik.marketing
  * License: GPL-3.0+
